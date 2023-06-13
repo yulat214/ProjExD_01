f0e32fed699eb59dabdd83ea7108711b1a1c8d06
yulat214 <shigineko64@yahoo.co.jp>
2023-06-13 15:41:38 +0900

    first commit

---
+++ pygame_image.py
@@ -0,0 +1,25 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
