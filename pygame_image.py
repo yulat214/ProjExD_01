1aff4ad314d85b9ff07146db4c14cf3d45394ab6
yulat214 <shigineko64@yahoo.co.jp>
2023-06-13 15:57:56 +0900

    4th commit

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(fw_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
